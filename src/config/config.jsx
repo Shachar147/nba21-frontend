18967ac0ef32cdf92fe5c8207f0498db66d7eaeb
Shacharr <Shacharr@centerity.com>
2021-01-18 23:05:17 +0200

    set shoot score to 0 by default + try to fix config for heroku

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,3 +1,3 @@
-export const SERVER_URL = (process.env.NODE_ENV === 'production') ? 'https://nba21-server.herokuapp.com : http://localhost';
-export const SERVER_PORT = (process.env.NODE_ENV === 'production') ? '' : '3000';
+export const SERVER_URL = (process.env.NODE_ENV === 'production') ? 'https://nba21-server.herokuapp.com' : 'http://localhost';
+export const SERVER_PORT = (process.env.NODE_ENV === 'production') ? '' : '3001';
 export const SERVER_ADDRESS = SERVER_URL + (SERVER_PORT !== '') ? ':' + SERVER_PORT : '';
