318bbddf915a30c465af913c9bd73b2c3432cdcd
Shacharr <Shacharr@centerity.com>
2021-01-19 15:31:48 +0200

    config jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,3 +1,1 @@
-export const SERVER_URL = (process.env.NODE_ENV === 'production') ? 'https://nba21-server.herokuapp.com' : 'http://localhost';
-export const SERVER_PORT = (process.env.NODE_ENV === 'production') ? '' : '3001';
-export const SERVER_ADDRESS = SERVER_URL + (SERVER_PORT !== '') ? ':' + SERVER_PORT : '';
+export const SERVER_ADDRESS = (process.env.NODE_ENV === 'development') ? 'http://localhost:3001' : 'https://nba21-server.herokuapp.com';
