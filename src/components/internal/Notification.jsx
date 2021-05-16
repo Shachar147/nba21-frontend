1bf34bdef7c9dceb364341647a1251763863facb
Shacharr <Shacharr@centerity.com>
2021-05-16 12:57:11 +0300

    remove unused things

--- src/components/internal/Notification.jsx
+++ src/components/internal/Notification.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {PLAYER_NO_PICTURE} from "../../helpers/consts";
-import DropdownInput from "../inputs/DropdownInput";
 
 export default class Notification extends React.Component {
 
