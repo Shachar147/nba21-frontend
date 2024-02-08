36ed604cbf0cf44cd98550c53a58278292875c40
Shachar Ratzabi Mac <Shachar147@gmail.com>
2024-02-08 12:55:29 +0200

    add total finals appearances stat

--- src/helpers/sort.js
+++ src/helpers/sort.js
@@ -252,7 +252,7 @@ export function totalGamesWithOTSort(a, b){
 
 export function totalFinalsPercentsSort(a, b) {
     function getPercents(settings){
-        calcPercents(settings['Total Finals Appearances'], settings['total_tournaments']);
+        calcPercents(settings['total_finals_appearances'], settings['total_tournaments']);
     }
 
     return specificSort('total', { "total": getPercents(a) }, { "total": getPercents(b) });
