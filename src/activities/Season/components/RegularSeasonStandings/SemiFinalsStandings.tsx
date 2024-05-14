43d41f7a0414bba359ad69aba7eb46dd1c06d323
Yahav Petel <yahavpetel@gmail.com>
2024-05-14 21:55:46 +0300

    fix series standints

--- src/activities/Season/components/RegularSeasonStandings/SemiFinalsStandings.tsx
+++ src/activities/Season/components/RegularSeasonStandings/SemiFinalsStandings.tsx
@@ -46,9 +46,14 @@ function SemiFinalsStandings({ stats, mode, teamsByName, store, max=8 }: SemiFin
     const seriesWithLogos: string[] = [];
     pSeries.forEach((team, idx) => {
         if (idx <= max/2 - 1) {
-            series.push(`${team} vs ${order[order.length - 1 - idx]}`);
+
+            const team1 = team;
+            const team2 = order[order.length - 1 - idx];
+
+            series.push(`${team} vs ${team2}`);
             // @ts-ignore
-            seriesWithLogos.push(`<div style="position:relative;"><div style="position:absolute; top:12.5px;">vs</div><div class="flex-col gap-4" style="position:relative; left:20px;">${getTeamCell(team)}${getTeamCell(order[order.length - 1 - idx])}</div></div>`)
+            // seriesWithLogos.push(`<div style="position:relative;"><div style="position:absolute; top:12.5px;">vs</div><div class="flex-col gap-4" style="position:relative; left:20px;">${getTeamCell(team)}${getTeamCell(team2)}</div></div>`)
+            seriesWithLogos.push(`<div style="position:relative;"><div style="position:absolute; top:12.5px;">vs</div><div class="flex-col gap-4" style="position:relative; left:20px;">${getTeamCell(team, stats[team]?.total_wins ?? 0, stats[team2]?.total_wins ?? 0)}${getTeamCell(team2, stats[team2]?.total_wins ?? 0, stats[team]?.total_wins ?? 0)}</div></div>`)
         }
     })
 
@@ -69,21 +74,26 @@ function SemiFinalsStandings({ stats, mode, teamsByName, store, max=8 }: SemiFin
             w_l = w_l.reverse();
         }
 
+        let sweepBlock = '';
+        let gameSevenBlock = '';
         if (stats[team1]?.total_wins == 4 || stats[team2]?.total_wins == 4) {
             w_l[1] = `<span class='strike-through nba-red-color'>${w_l[1]}</span>`;
 
             if (w_l[1].includes("0W")){
-                w_l[1] += '&nbsp;&nbsp; <span class="color-brown">Sweep! &nbsp;🧹</span>';
+                // w_l[1] += '&nbsp;&nbsp; <span class="color-brown">Sweep! &nbsp;🧹</span>';
+                sweepBlock = '<span class="color-brown">Sweep! &nbsp;🧹</span>';
             } else if (w_l[1].includes("3W")) {
-                w_l[1] += '&nbsp;&nbsp; <span class="color-yellow">Game 7! &nbsp;💪</span>';
+                // w_l[1] += '&nbsp;&nbsp; <span class="color-yellow">Game 7! &nbsp;💪</span>';
+                gameSevenBlock = '<span class="color-yellow">Game 7! &nbsp;💪</span>';
             }
         }
 
         const _idx = rOrder.findIndex((s) => s == team1);
         const _idx2 = rOrder.findIndex((s) => s == team2);
         standingStats[`${_idx+1}${nth(_idx+1)} vs ${_idx2 + 1}${nth(_idx2 + 1)}`] = [
+            // standingStats[`${idx+1}${nth(idx+1)} vs ${8-idx}${nth(8-idx)}`] = [
             seriesWithLogos[idx],
-            `<span class="font-weight-normal">${w_l.length == 0 ? 'Not started yet' : w_l.join("&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;")}</span>`,
+            `<span class="font-weight-normal">${w_l.length == 0 ? 'Not started yet' : sweepBlock.length + gameSevenBlock.length == 0 ? '-' : `${sweepBlock}${gameSevenBlock}`}</span>`,
             `${team1}<br/><span class="font-weight-normal">${w_l.length == 0 ? "-" : getMvps(stats[team1]?.records ?? [], team1)}</span>`,
             `${team2}<br/><span class="font-weight-normal">${w_l.length == 0 ? "-" : getMvps(stats[team2]?.records ?? [], team2)}</span>`
         ];
@@ -93,10 +103,15 @@ function SemiFinalsStandings({ stats, mode, teamsByName, store, max=8 }: SemiFin
         return (x.score1 > x.score2 && x.team1_name == teamName) || (x.score2 > x.score1 && x.team2_name == teamName);
     }
 
-    function getTeamCell(teamName: string): string {
+    function getTeamCell(teamName: string, totalWins: number, otherTeamWins: number): string {
         const teamLogo = teamsByName[teamName].logo;
 
-        return `<div class="flex-row align-items-center"><img src=${teamLogo} width="24" height="24" class="border-50-percents"/> ${teamName}</div>`;
+        const teamCell = `<div class="flex-row align-items-center"><img src=${teamLogo} width="24" height="24" class="border-50-percents"/> ${teamName} (${totalWins}W)</div>`;
+        if (otherTeamWins == 4){
+            return `<span class='strike-through nba-red-color'>${teamCell}</span>`;
+        }
+
+        return teamCell;
     }
 
     function getMvps(records: ISeasonGame[], teamName: string): string{
@@ -124,7 +139,7 @@ function SemiFinalsStandings({ stats, mode, teamsByName, store, max=8 }: SemiFin
         <div>
             <div className="ui header margin-top-10">{mode} Standings</div>
             <StatsTableInner
-                cols={['#', 'Series', 'Standing', 'MVPs 1', 'MVPs 2']}
+                cols={['#', 'Series', 'MVPs 1', 'MVPs 2']}
                 stats={standingStats}
                 showMoreOpened={true}
                 switchMaxNumber={10}
