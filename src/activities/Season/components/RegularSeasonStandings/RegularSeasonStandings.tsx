84afbcd3ad7dcf5ed70515ba1032ee85ade158ef
Yahav Petel <yahavpetel@gmail.com>
2024-06-19 17:04:22 +0300

    wip

--- src/activities/Season/components/RegularSeasonStandings/RegularSeasonStandings.tsx
+++ src/activities/Season/components/RegularSeasonStandings/RegularSeasonStandings.tsx
@@ -158,7 +158,7 @@ function RegularSeasonStandings({ stats, teamsData, mode, teamsByName, store }: 
                     {
                         sorted_mvps.map((playerName) => `${playerName} (${details[playerName]['mvps']}w / ${details[playerName]['diff']}+- / ${details[playerName]['comebacks']}cb / ${details[playerName]['overtimes']}ot / ${details[playerName]['knockouts']}ko)`).slice(0, 3).join(", ")}
                 </div>
-                {(currMode != "Finals" && !isSeasonOver) && <div className="flex-row width-100-percents justify-content-center opacity-06">
+                {(currMode != "Finals" && !isSeasonOver) && !!other_two.length && <div className="flex-row width-100-percents justify-content-center opacity-06">
                     <b>On the race:</b> &nbsp;{
                     other_two.map((x) => `${x} (${mvps[x]} pts)`).slice(0, 3).join(", ")}
                 </div>}
